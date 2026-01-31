dffac10853cf360c56326bd3d457026a95e929b7
kalyan <kalyanjkr@gmail.com>
2026-01-31 12:15:57 +0530

    actions

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Rudresh :: hi nandana, how are you?"
-echo "nandana :: hi rudresh, i'm fine. what about you?"
-echo "rudresh :: good, what are you doing?"
-echo "Nandana :: i'm getting ready to go to school"
+echo "Rudresh:: hi nandana, how are you?"
+echo "nandana:: hi rudresh, i'm fine. what about you?"
+echo "rudresh:: good, what are you doing? "
+echo "Nandana:: i'm getting ready to go to school"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Rudresh # no space between value and =
+Person2=Nandana
+
+echo "$Person1:: hi $Person2, how are you?"
+echo "$Person2:: hi $Person1, i'm fine. what about you?"
+echo "$Person1:: good, what are you doing? "
+echo "$Person2:: i'm getting ready to go to school"
