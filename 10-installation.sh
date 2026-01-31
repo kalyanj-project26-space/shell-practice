089b780583f2b0f4536254f9069b171ccaf6ae28
kalyan <kalyanjkr@gmail.com>
2026-01-31 16:30:06 +0530

    actions

---
+++ 10-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "please run this script with root user access"
+  exit 1
+fi
+
+echo "installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+ echo "installing nginx ... FAILURE"
+ exit 1
+else
+ echo "installing nginx ... SUCCESS"
+fi
