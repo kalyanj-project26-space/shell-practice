513a1c9580d653dff538b3d74b3acb8756987d7e
kalyan <kalyanjkr@gmail.com>
2026-01-31 14:28:07 +0530

    actions

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ##special variables##
+
  echo "all arguments passed: $@"
  echo "number of variables passed to script: $#"
  echo "script name: $0"
- echo "present working director: $PWD"
- echo "who is running the script: $USER"
- echo "home directory of the user: $HOME"
+ echo "present working directory: $PWD"
+ echo "who is running: $USER"
+ echo "home directory of the current user: $HOME"
  echo "PID of script: $$"
  sleep 100 &
  echo "PID of recently executed background script: $!"
