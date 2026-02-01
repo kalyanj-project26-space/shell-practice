ea7a23a1d678a060611be02a1f34f676053e238b
kalyan <kalyanjkr@gmail.com>
2026-02-01 08:51:03 +0530

    actions

--- 14-colors.sh
+++ 14-colors.sh
@@ -16,10 +16,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-       echo "$2 .... $R FAILURE $N" | tee -a $LOGS_FILE
+       echo -e "$2 .... $R FAILURE $N" | tee -a $LOGS_FILE
        exit 1
     else
-       echo "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+       echo  -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 } 
 
@@ -31,6 +31,6 @@ do
      dnf install $package -y &>>$LOGS_FILE
      VALIDATE $? "$package installation"
   else
-     echo "$package already installed, $Y skipping $N"
+     echo -e "$package already installed, $Y skipping $N"
   fi
 done
