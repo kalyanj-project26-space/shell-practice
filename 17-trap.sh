d3478623da1dd8d3a05a76928f902bd210bfbdf5
kalyan <kalyanjkr@gmail.com>
2026-02-08 12:07:57 +0530

    changed something

---
+++ 17-trap.sh
@@ -0,0 +1,32 @@
+set -e
+trap 'echo "there is an error in $LINENO, command: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[0m'
+B='\e[34m'
+if [ $USERID -ne 0 ]; then
+  echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+
+
+for package in $@ # sudo sh 13-logs.sh nginx mysql nodejs
+do
+  dnf list installed $package &>>$LOGS_FILE
+  if [ $? -ne 0 ]; then
+     echo "$package not installed, installing now"
+     dnf install $package -y &>>$LOGS_FILE
+     
+  else
+     echo -e "$package already installed, $Y skipping $N"
+  fi
+done
